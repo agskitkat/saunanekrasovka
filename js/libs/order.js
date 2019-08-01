4d6417a2e7b3ef91d055bafe3e1c8e6b4b4a55ba
Oxibug <agskitkat@gmail.com>
2019-08-01 13:05:14 +0300

    Calendar

--- js/libs/order.js
+++ js/libs/order.js
@@ -1,9 +1,44 @@
 $(function() {
+    var date_start = false;
+    var date_stop  = new Date();
+
+    $(".calendar").calendar(function(date){
+        date_start = date;
+    });
+
     $(".time-picker").timepicker();
-    $(".calendar").calendar();
+
     var steps = 1;
     var end_steps = 4;
+
     $(".js-target-next-step").click(function(){
-        $(this).closest(".steps")
+
+        var steps_container = $(this).closest(".steps");
+        steps_container.find(".step_active").removeClass("step_active");
+
+        switch(steps) {
+            case 1:
+
+                break;
+            case 2:
+
+                break;
+            case 3:
+
+                break;
+            case 4:
+
+                break;
+        }
     });
+
+    $(".js-target-back-step").click(function(){
+        if(steps === 1) {
+            $(".modal.js-target-order").removeClass("active");
+        }
+    });
+
+    function setStepInfo() {
+
+    }
 });
